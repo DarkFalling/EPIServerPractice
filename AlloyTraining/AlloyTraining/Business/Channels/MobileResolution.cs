c488c920f62ced4c60092f0d546b3a217c4dc5b4
David Long <davidlong@objectivasoftware.com>
2016-11-29 21:56:38 +0800

    Exercise C4 - Create display channel for Mobile

---
+++ AlloyTraining/AlloyTraining/Business/Channels/MobileResolution.cs
@@ -0,0 +1,44 @@
+﻿using EPiServer.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloyTraining.Business.Channels
+{
+    public class MobileResolution : IDisplayResolution
+    {
+        public int Height
+        {
+            get
+            {
+                return 480;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return this.GetType().FullName;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Moblie(320*480)";
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return 320;
+            }
+        }
+    }
+}

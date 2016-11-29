e6e698c8645d10697854742a0bb965fa8717089d
David Long <davidlong@objectivasoftware.com>
2016-11-29 21:09:26 +0800

    Exercise C3 - Tag the partial template to only render in tagged content area

--- AlloyTraining/AlloyTraining/Models/Pages/ProductPage.cs
+++ AlloyTraining/AlloyTraining/Models/Pages/ProductPage.cs
@@ -16,5 +16,15 @@ namespace AlloyTraining.Models.Pages
             Order = 305)]
         [UIHint(UIHint.Textarea)]
         public virtual string UniqueSellingPoints { get; set; }
+
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 320)]
+        public virtual ContentArea MainContentArea { get; set; }
+
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 330)]
+        public virtual ContentArea RelatedContentArea { get; set; }
     }
 }

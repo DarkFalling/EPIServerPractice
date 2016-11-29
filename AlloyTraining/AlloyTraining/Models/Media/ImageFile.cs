c522d73bfe4b9520bffd7d7a43fa0e574fd77160
David Long <davidlong@objectivasoftware.com>
2016-11-29 18:11:52 +0800

    Exercise B5-Creating a New Layout Page, a Product Page Type and a Product Controller and View

---
+++ AlloyTraining/AlloyTraining/Models/Media/ImageFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "f13946b5-05fe-4326-9754-f816fe9328c1", Description = "")]
+    /*[MediaDescriptor(ExtensionString = "pdf,doc,docx")]*/
+    public class ImageFile : ImageData
+    {
+        [CultureSpecific]
+        [Editable(true)]
+        [Display(
+            Name = "Description",
+            Description = "Description field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Description { get; set; }
+    }
+}
---
+++ AlloyTraining/AlloyTraining/Models/Pages/ProductPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "ProductPage", GUID = "e39d02f4-d137-4981-bbb6-e79cafb7aaf1", Description = "Alloy Product Page")]
+    public class ProductPage : StandardPage
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 305)]
+        [UIHint(UIHint.Textarea)]
+        public virtual string UniqueSellingPoints { get; set; }
+    }
+}

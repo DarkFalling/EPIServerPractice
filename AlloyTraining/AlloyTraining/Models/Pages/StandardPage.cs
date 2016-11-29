f72de725f36a30b39682663ea12f6e56f1037c76
David Long <davidlong@objectivasoftware.com>
2016-11-29 17:04:52 +0800

    Exercise B4- Creating a New Layout Page, a Standard Page Type and a Standard Page Controller and View

---
+++ AlloyTraining/AlloyTraining/Models/Pages/StandardPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "0a519ec1-9a2d-4a07-a896-2c69681bed72", Description = "Alloy Standard Page")]
+    public class StandardPage : SitePageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 310)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}

d0b10ab62be86a32dea653b1b6455fa5e97d33f6
David Long <sevensetup@hotmail.com>
2016-11-30 15:18:07 +0800

    E1 - Create a Page Listing Block

---
+++ AlloyTraining/AlloyTraining/Models/Blocks/ListingBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "ListingBlock", GUID = "12746732-5e2b-4549-88d4-726b1eb9dc07", Description = "")]
+    public class ListingBlock : BlockData
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        public virtual string Heading { get; set; }
+
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 200)]
+        public virtual PageReference RootPage { get; set; }
+
+    }
+}
---
+++ AlloyTraining/AlloyTraining/Models/ViewModels/ListingBlockModel.cs
@@ -0,0 +1,16 @@
+﻿using AlloyTraining.Models.Pages;
+using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class ListingBlockModel
+    {
+        public string Heading { get; set; }
+        public IEnumerable<PageData> Items { get; set; }
+    }
+}

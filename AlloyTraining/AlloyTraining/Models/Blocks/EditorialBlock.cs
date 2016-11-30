759dd2f04f49b4a15d8bddb739ef857bb31078cc
David Long <sevensetup@hotmail.com>
2016-11-30 11:11:11 +0800

    Exercise D1 - Create a Block without a Controller

---
+++ AlloyTraining/AlloyTraining/Models/Blocks/EditorialBlock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "EditorialBlock", GUID = "9b3cf771-ab17-4945-b4f1-417136d664d8", Description = "")]
+    public class EditorialBlock : BlockData
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}

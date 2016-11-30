e5f2f8258d763114f9307704e4aa8e239f160eb9
David Long <sevensetup@hotmail.com>
2016-11-30 14:02:29 +0800

    Exxercise D3 - Create a Preview Renderer for Blocks

---
+++ AlloyTraining/AlloyTraining/Models/Pages/PreviewBlock.cs
@@ -0,0 +1,26 @@
+﻿using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloyTraining.Models.Pages
+{
+    public class PreviewBlock : PageData
+    {
+        public IContent PreviewContent { get; set; }
+        public ContentArea ContentArea { get; set; }
+
+        public PreviewBlock(PageData currentPage, IContent previewContent)
+            : base(currentPage)
+        {
+            this.PreviewContent = previewContent;
+            this.ContentArea = new ContentArea();
+            this.ContentArea.Items.Add(new ContentAreaItem
+            {
+                ContentLink = this.PreviewContent.ContentLink
+            });
+        }
+    }
+}
---
+++ AlloyTraining/AlloyTraining/Models/ViewModels/BlockEditPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AlloyTraining.Models.Pages;
+using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class BlockEditPageViewModel : IPageViewModel<SitePageData>
+    {
+        public BlockEditPageViewModel(PageData pageData, IContent content)
+        {
+            previewBlock = new PreviewBlock(pageData, content);
+            CurrentPage = pageData as SitePageData;
+        }
+
+        public PreviewBlock previewBlock { get; set; }
+        public SitePageData CurrentPage { get; set; }
+    }
+}

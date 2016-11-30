746ae0aed218ee7535d7c3214bd16db7471f1bae
David Long <sevensetup@hotmail.com>
2016-11-30 17:33:12 +0800

    Exercise F1 - Create a search page

---
+++ AlloyTraining/AlloyTraining/Models/Pages/SearchPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "SearchPage", GUID = "ac9eb7ab-2e3a-4fa9-abeb-b7e47e80625b", Description = "")]
+    public class SearchPage : SitePageData
+    {
+    }
+}
